be5a397f4bbe2d2eb88503c88e9a7e60e09c6603
XiiXZz <pieter.goossens@student.odisee.be>
2021-06-18 15:05:01 +0000

    Task 3 done

--- Task 7/check_ios.sh
+++ Task 7/check_ios.sh
@@ -6,11 +6,11 @@ sed -n 15p ios_configs/running_config_csr101.txt
 sed -n 8p ios_configs/running_config_csr101.txt
 str=$(sed -n 8p ios_configs/running_config_csr101.txt)
 substr=$(echo $str| cut -b 14-16)
-if [ "$subst" == "101" ]
+if [ "$substr" == "101" ]
 	then
 		echo ios needs upgrading
 fi
-if [ "$subst" == "111" ] 
+if [ "$substr" == "111" ] 
 then 
         echo "ios is up to date"
 fi
@@ -19,10 +19,10 @@ sed -n 15p ios_configs/running_config_csr111.txt
 sed -n 8p ios_configs/running_config_csr111.txt
 str=$(sed -n 8p ios_configs/running_config_csr111.txt)
 substr=$(echo $str| cut -b 14-16)
-if [ "$subst" == "101" ]; then
+if [ "$substr" == "101" ]; then
         echo ios needs upgrading
 fi
-if [ "$subst" == "111" ] 
+if [ "$substr" == "111" ] 
 then 
         echo "ios is up to date"
 fi
